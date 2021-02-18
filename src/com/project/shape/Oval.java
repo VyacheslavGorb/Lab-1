9ba6f941c5de38929d8e6f11b99b6cb1782a82d4
Your Name <you@example.com>
2021-02-18 13:31:10 +0300

    Create Triangle class

---
+++ src/com/project/shape/Oval.java
@@ -0,0 +1,22 @@
+package com.project.shape;
+
+import java.awt.*;
+
+public class Oval extends BoundedShape{
+    public Oval() {
+        super();
+    }
+
+    public Oval(Point startPoint, Point endPoint) {
+        super(startPoint, endPoint);
+    }
+
+    public Oval(int x1, int y1, int x2, int y2) {
+        super(x1, y1, x2, y2);
+    }
+
+    @Override
+    public void draw(Graphics graphics) {
+        graphics.drawOval(startPoint.x, startPoint.y, getWidth(),getHeight());
+    }
+}
