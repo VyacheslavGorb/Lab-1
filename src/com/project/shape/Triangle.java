547a2a5d34d5e2ea1026d2348abd7a1735044298
Your Name <you@example.com>
2021-02-18 13:27:48 +0300

    Create Triangle class

---
+++ src/com/project/shape/Triangle.java
@@ -0,0 +1,24 @@
+package com.project.shape;
+
+import java.awt.*;
+
+public class Triangle extends BoundedShape {
+    public Triangle() {
+        super();
+    }
+
+    public Triangle(Point startPoint, Point endPoint) {
+        super(startPoint, endPoint);
+    }
+
+    public Triangle(int x1, int y1, int x2, int y2) {
+        super(x1, y1, x2, y2);
+    }
+
+    @Override
+    public void draw(Graphics graphics) {
+        graphics.drawLine(startPoint.x, endPoint.y, startPoint.x + getWidth() / 2, startPoint.y);
+        graphics.drawLine(startPoint.x + getWidth() / 2, startPoint.y, endPoint.x, endPoint.y);
+        graphics.drawLine(startPoint.x, endPoint.y, endPoint.x, endPoint.y);
+    }
+}
